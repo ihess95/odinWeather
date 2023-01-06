d6854823fef51948cdd92957a51b17a7a698870a
Ian Hess <ian.h.hess@gmail.com>
2023-01-06 13:33:34 -0500

    add key

--- src/weatherApiMod.js
+++ src/weatherApiMod.js
@@ -1,6 +1,7 @@
 const weatherData = "";
 const lat = 90.92;
 const lon = 90.9;
+const apiKey = "5ac36be232f53df626408888c0e28c2e";
 async function getWeather() {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`,
