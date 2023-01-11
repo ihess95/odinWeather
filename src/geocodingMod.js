24b6ef6f684d768fb45cf453dc3b8e5d99b88e2d
Ian Hess <ian.h.hess@gmail.com>
2023-01-11 14:39:31 -0500

    log errors

--- src/geocodingMod.js
+++ src/geocodingMod.js
@@ -3,9 +3,12 @@ async function geoCoder(location, apiKey) {
     `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`,
     { mode: "cors" }
   );
-  console.log(JSON.stringify(response));
-  const lat = response.lat;
-  const lon = response.lon;
+  const locationData = response.json();
+  console.log("data" + JSON.stringify(locationData));
+  const lat = locationData.lat;
+  console.log("lat" + lat);
+  const lon = locationData.lon;
+  console.log("lon" + lon);
 }
 
 export { geoCoder };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { getWeather } from "./weatherApiMod";
 import { geoCoder } from "./geocodingMod";
-console.log("test");
 
 const apiKey = "5ac36be232f53df626408888c0e28c2e";
 
 const mainBody = document.querySelector(".mainBody");
 mainBody.textContent = "test";
 
-getWeather(apiKey);
 geoCoder("London", apiKey);
+getWeather(apiKey);
