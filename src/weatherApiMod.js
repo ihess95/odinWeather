440b5c7ef2fe6b8dfc7ef60567968bafbc67ba99
Ian Hess <ian.h.hess@gmail.com>
2023-01-13 14:09:37 -0500

    clean up variables

--- src/weatherApiMod.js
+++ src/weatherApiMod.js
@@ -1,5 +1,3 @@
-const lat = "43.0718";
-const lon = "70.7626";
 async function getWeather(location, apiKey) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`,
