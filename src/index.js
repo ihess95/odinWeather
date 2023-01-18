08c9c1b7dc3bc1fe59dbce4873350e7374db3ff1
Ian Hess <ian.h.hess@gmail.com>
2023-01-18 11:08:54 -0500

    add try/catch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { getWeather } from "./weatherApiMod";
 
 const apiKey = "5ac36be232f53df626408888c0e28c2e";
-let weatherData = "";
 
 const mainBody = document.querySelector(".mainBody");
 mainBody.textContent = "test";
 
 getWeather("London", "imperial", apiKey);
-const coordinates = document.createElement("div");
-coordinates.textContent = weatherData;
-mainBody.appendChild(coordinates);
-console.log("from index" + weatherData.name);
+// const coordinates = document.createElement("div");
+// coordinates.textContent = weatherData;
+// mainBody.appendChild(coordinates);
+console.log("from index");
