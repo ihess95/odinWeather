2a2f94861997ba3becbb5717770c00006979a0d2
Ian Hess <ian.h.hess@gmail.com>
2023-01-18 10:50:26 -0500

    add variable for units

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const apiKey = "5ac36be232f53df626408888c0e28c2e";
 const mainBody = document.querySelector(".mainBody");
 mainBody.textContent = "test";
 
-getWeather("London", apiKey);
+getWeather("London", "imperial", apiKey);
 const coordinates = document.createElement("div");
 coordinates.textContent = weatherData;
 mainBody.appendChild(coordinates);
--- src/weatherApiMod.js
+++ src/weatherApiMod.js
@@ -1,12 +1,13 @@
-async function getWeather(location, apiKey) {
+async function getWeather(location, unit, apiKey) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=${apiKey}`,
     { mode: "cors" }
   );
   console.log(response);
   const weatherData = await response.json();
   console.log(`weatherData: ${weatherData.weather[0].main}`);
   console.log(`Temp: ${weatherData.main.temp}`);
+  console.log(`Feels Like: ${weatherData.main.feels_like}`);
   console.log(`Humidity: ${weatherData.main.humidity}`);
   console.log(`Wind: ${weatherData.wind.speed} MPH`);
   return weatherData;
