f7a23fc2391c7fdf12173e0c83c620ea149dd82a
Ian Hess <ian.h.hess@gmail.com>
2023-01-19 13:18:15 -0500

    clean up, realize typo in getWeather call

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@
 // import { weatherData } from "./weatherApiMod";
 
 const apiKey = "5ac36be232f53df626408888c0e28c2e";
-
-const mainBody = document.querySelector(".mainBody");
-mainBody.textContent = "test";
-
 const weatherData = "";
 
 async function getWeather(location, unit, apiKey) {
@@ -27,9 +23,9 @@ async function getWeather(location, unit, apiKey) {
   }
 }
 
-getweather("London", "Imperial", apiKey);
+getWeather("London", "Imperial", apiKey);
 
 // const coordinates = document.createElement("div");
 // coordinates.textContent = weatherData;
 // mainBody.appendChild(coordinates);
-console.log("from index" + weatherDataApp);
+console.log("from index" + weatherData);
