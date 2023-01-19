1af55881619385b48ea0f120c30440e02c4bb639
Ian Hess <ian.h.hess@gmail.com>
2023-01-19 13:14:20 -0500

    troubleshoot running function inside index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import { getWeather } from "./weatherApiMod";
+// import { getWeather } from "./weatherApiMod";
+// import { weatherData } from "./weatherApiMod";
 
 const apiKey = "5ac36be232f53df626408888c0e28c2e";
 
 const mainBody = document.querySelector(".mainBody");
 mainBody.textContent = "test";
 
-getWeather("London", "imperial", apiKey);
+const weatherData = "";
+
+async function getWeather(location, unit, apiKey) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=${apiKey}`,
+      { mode: "cors" }
+    );
+    console.log(response);
+    weatherData = await response.json();
+    console.log(weatherData.name);
+    console.log(`weatherData: ${weatherData.weather[0].main}`);
+    console.log(`Temp: ${weatherData.main.temp}`);
+    console.log(`Feels Like: ${weatherData.main.feels_like}`);
+    console.log(`Humidity: ${weatherData.main.humidity}`);
+    console.log(`Wind: ${weatherData.wind.speed} MPH`);
+  } catch {
+    console.log("error");
+  }
+}
+
+getweather("London", "Imperial", apiKey);
+
 // const coordinates = document.createElement("div");
 // coordinates.textContent = weatherData;
 // mainBody.appendChild(coordinates);
-console.log("from index");
+console.log("from index" + weatherDataApp);
